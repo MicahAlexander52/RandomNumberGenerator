3a1d9ffc90fed6d764c67dbef58b3db2f1237d9b
MicahAlexander52 <127165971+MicahAlexander52@users.noreply.github.com>
2023-03-26 19:54:51 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int number;
+Console.WriteLine("Random Number Generator!");
+Console.WriteLine("Press any key to continue >>>>>");
+Console.ReadKey();
+Random random = new Random();
+number = random.Next(0, 100);
+Console.WriteLine("Random Number: " + number);
+Console.ReadLine(); 
